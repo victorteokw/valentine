ef6ff7d1224e3745212007e19f920b4c85277a8a
Zhang Kaiyu <yeannylam@gmail.com>
2019-02-13 21:09:21 +0800

    fix bugs

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -31,7 +31,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
         options: {
-          presets: ["env", "react", "stage-0"]
+          presets: ["@babel/env", "@babel/react"]
         }
       },
       {
